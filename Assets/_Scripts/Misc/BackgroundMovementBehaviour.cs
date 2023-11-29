1d8480682d658424decb09ff7588e7bfa403ddbb
ZenMar2000 <enrico.marzola2000@gmail.com>
2023-11-29 11:34:13 +0100

    Added parallax background

--- Assets/_Scripts/Misc/BackgroundMovementBehaviour.cs
+++ Assets/_Scripts/Misc/BackgroundMovementBehaviour.cs
@@ -4,19 +4,40 @@ public class BackgroundMovementBehaviour : MonoBehaviour
 {
     public float ScrollSpeed = 2f;
     public float MaxHeightBeforeRepeat = -240;
-
+    public float lerpValue = 0.5f;
+    private Rigidbody2D player;
     private Rigidbody2D rb;
 
-    private void Awake()
+    private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        rb.velocity = new Vector2 (0, ScrollSpeed * -1);
+        rb.velocity = new Vector2(0, ScrollSpeed * -1);
+        player = GameInfo.Player.GetComponentInChildren<Rigidbody2D>();
     }
-    private void FixedUpdate()
+
+    private void Update()
     {
-        if(transform.position.y <= MaxHeightBeforeRepeat)
+        if (transform.position.y <= MaxHeightBeforeRepeat)
         {
-            transform.position = new Vector2 (0, 0);
+            transform.position = new Vector2(0, 0);
+        }
+
+        if (player != null || player.velocity.x != 0)
+        {
+
+            if (player.velocity.x > 0)
+            {
+                rb.velocity = Vector2.Lerp(rb.velocity, new Vector2(ScrollSpeed * -1 * 3, rb.velocity.y), lerpValue);
+            }
+            else if (player.velocity.x < 0)
+            {
+                rb.velocity = Vector2.Lerp(rb.velocity, new Vector2(ScrollSpeed * 3, rb.velocity.y), lerpValue);
+
+            }
+        }
+        else
+        {
+            rb.velocity = Vector2.Lerp(rb.velocity, new Vector2(0, rb.velocity.y), lerpValue);
         }
     }
 }
